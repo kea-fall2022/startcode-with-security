6eead81ad2440f3cf0450aaf0f58e8497e85e7e2
plaul <lmor@kea.dk>
2022-08-30 17:36:52 +0200

    full CRUD

--- src/main/java/dat3/cars/service/MemberService.java
+++ src/main/java/dat3/cars/service/MemberService.java
@@ -71,6 +71,6 @@ public class MemberService {
   }
 
   public void deleteByUsername(String username) {
-    memberRepository.existsById(username);
+    memberRepository.deleteById(username);
   }
 }
